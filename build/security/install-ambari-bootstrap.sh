1abe3331843999e4eeb04d9ffb0936caa2efb9fd
WilliamGonzalez <wgonzalez@hw11715.local>
2016-11-03 19:09:42 -0400

    Cloning Seanorama to HWX U
    Cloning Seanorama to HWX U

--- build/security/install-ambari-bootstrap.sh
+++ build/security/install-ambari-bootstrap.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
+#sudo yum -y -q install git
+#sudo git clone https://github.com/HortonworksUniversity/Ops_Labs/1.1.0/build/security/ambari-bootstrap /opt/ambari-bootstrap
+#sudo chmod -R g+rw /opt/ambari-bootstrap
+#sudo chown -R ${USER}:users /opt/ambari-bootstrap
+#ln -s /opt/ambari-bootstrap ~/
+
+
 sudo yum -y -q install git
-sudo git clone https://github.com/HortonworksUniversity/Ops_Labs/1.1.0/build/security/ambari-bootstrap /opt/ambari-bootstrap
+
+sudo git clone -b 1.1.0 https://github.com/HortonworksUniversity/Ops_Labs /opt/ambari-bootstrap
+sudo git filter-branch --prune-empty --subdirectory-filter build/security/ambari-bootstrap-master
+
 sudo chmod -R g+rw /opt/ambari-bootstrap
 sudo chown -R ${USER}:users /opt/ambari-bootstrap
 ln -s /opt/ambari-bootstrap ~/
